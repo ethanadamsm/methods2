66f2c11684760724064cdd08b17295ac058dc4b9
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-08-28 10:20:58 -0600

    Added 2 methods

--- methods2.rb
+++ methods2.rb
@@ -1,12 +1,55 @@
 module Methods2
 	
-	# TODO - write elevenish?
+	def elevenish?(n)
+		if n % 11 == 0 || (n - 1) % 11 == 0
+			true
+		else
+			false
+		end
+	end
 	
-	# TODO - write ice_cream_party?
+	def ice_cream_party?(a, b)
+		if a < 5 && b < 5 
+			if a / b == 2 || b / a == 2
+				2
+			else
+				0
+			end
+		elsif a / b == 2 || b / a == 2 #&& a >= 5 && b >= 5
+			2
+		else
+			1
+		end
+	end
 	
-	# TODO - write successful_squirrel_party?
+	def successful_squirrel_party?(a, b)
+		if b < 5
+			if a >= 40 && a <= 60
+				true
+			else
+				false
+			end
+		elsif b >= 5 && b < 8
+			if a >= 40
+				true
+			else
+				false
+			end
+		end
+	end
 	
-	# TODO - write ticket
+	def ticket(a, b, c)
+		ab = a + b
+		ac = a + c
+		bc = b + c
+		if ab == 10 || ac == 10 || bc == 10
+			10
+		elsif ab - ac == 10 || ab - bc == 10 || ac - bc == 10 || ac - ab == 10 || bc - ac == 10 || bc - ab == 10	
+			5
+		else
+			0		
+		end
+	end
 
 	# TODO - write in_order?
 
--- methods2_test.rb
+++ methods2_test.rb
@@ -2,13 +2,55 @@ require 'minitest/autorun'
 require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-require_relative 'methods'
+require_relative 'methods2'
 
 class Methods2Test < MiniTest::Test
 	def setup
 		@m = Class.new do
-     include Methods
+     include Methods2
    	end.new
+	end
+
+	def test_elevenish
+		assert_equal true, @m.elevenish?(11)
+		assert_equal true, @m.elevenish?(12)
+		assert_equal true, @m.elevenish?(22)
+		assert_equal true, @m.elevenish?(23)
+		assert_equal false, @m.elevenish?(15)
+		assert_equal false, @m.elevenish?(17)
+	end
+
+	def test_ice_cream_party
+		assert_equal 0, @m.ice_cream_party?(1, 1)
+		assert_equal 0, @m.ice_cream_party?(4, 3)
+		assert_equal 1, @m.ice_cream_party?(6, 7)
+		assert_equal 1, @m.ice_cream_party?(5, 5)
+		assert_equal 2, @m.ice_cream_party?(2, 4)
+		assert_equal 2, @m.ice_cream_party?(5, 10)
+		assert_equal 2, @m.ice_cream_party?(10, 5)
+		assert_equal 2, @m.ice_cream_party?(6, 12)
+	end
+
+	def test_successful_squirrel_party
+		assert_equal true, @m.successful_squirrel_party?(40, 0)
+		assert_equal true, @m.successful_squirrel_party?(60, 0)
+		assert_equal true, @m.successful_squirrel_party?(40, 3)
+		assert_equal true, @m.successful_squirrel_party?(40, 3)
+		assert_equal true, @m.successful_squirrel_party?(45, 4)
+		assert_equal true, @m.successful_squirrel_party?(45, 4)
+		assert_equal true, @m.successful_squirrel_party?(61, 5)
+		assert_equal true, @m.successful_squirrel_party?(61, 6)
+		assert_equal false, @m.successful_squirrel_party?(39, 0)
+		assert_equal false, @m.successful_squirrel_party?(14, 0)
+	end
+
+	def test_ticket
+		assert_equal 10, @m.ticket(5, 5, 5)
+		assert_equal 5, @m.ticket(5, 5, 0)
+		assert_equal 5, @m.ticket(0, 5, 5)
+		assert_equal 5, @m.ticket(5, 0, 5)
+		assert_equal 0, @m.ticket(2, 2, 2)
+		assert_equal 0, @m.ticket(4, 3, 9)
 	end
 
 end
